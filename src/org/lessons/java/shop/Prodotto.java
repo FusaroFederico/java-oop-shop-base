859b17e4330980c986272252fa1f830e0ecd539b
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-06-12 15:03:14 +0200

    add constructor method to Prodotto.java
    renamed instance variables and add constructor method

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,10 +1,20 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
+	
 	// instance variables
-	int code;
-	String name;
-	String description;
-	float price;
-	int iva;
+	int itemCode;
+	String itemName;
+	String itemDescription;
+	float itemPrice;
+	int itemIva;
+	
+	// constructor method
+	public Prodotto(int code, String name, String description, float price, int iva) {
+		itemCode = code;
+		itemName = name;
+		itemDescription = description;
+		itemPrice = price;
+		itemIva = iva;
+	}
 }
